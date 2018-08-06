13eea200fc2c155e46bc538ad706719f78bd1eb4
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-08-06 05:47:07 +0300

    Add files via upload

--- src/MainPage.js
+++ src/MainPage.js
@@ -14,9 +14,13 @@ class MainPage extends Component{
                             <h2 className="bookshelf-title">Currently Reading</h2>
                             <div className="bookshelf-books">
                                 <ol className="books-grid">
-                                  <li>
-                                      <Book />
-                                  </li>
+                                  {this.props.books.filter(book => book.shelf === 'currentlyReading')
+                                  .map(book => (
+                                      <li key={book.id}>
+                                          <Book book={book} />
+                                      </li>
+                                  ))
+                                  }
                                 </ol>
                             </div>
                         </div>
@@ -27,10 +31,10 @@ class MainPage extends Component{
                               {this.props.books.filter(book => book.shelf === 'wantToRead')
                               .map(book => (
                                   <li key={book.id}>
-                                      <Book />
+                                      <Book book={book} />
                                   </li>
                               ))
-                            }
+                              }
                             </ol>
                           </div>
                         </div>
@@ -38,9 +42,13 @@ class MainPage extends Component{
                           <h2 className="bookshelf-title">Read</h2>
                           <div className="bookshelf-books">
                             <ol className="books-grid">
-                              <li>
-                                  <Book />
-                              </li>
+                              {this.props.books.filter(book => book.shelf === 'read')
+                              .map(book => (
+                                  <li key={book.id}>
+                                      <Book book={book} />
+                                  </li>
+                              ))
+                              }
                             </ol>
                           </div>
                         </div>
