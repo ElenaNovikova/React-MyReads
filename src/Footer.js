d88c57856f1cc8176526586d5fec0ac89008684e
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-08-28 08:43:42 +0100

    fix: Style attribute
    prevents XSS security holes, which could appear while using style attribute with a property as a CSS string

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
     render() {
+
+        const divStyle = {
+            textAlign: "center",
+            fontSize: "0.9em",
+        };
+
         return (
             <footer className="copyright">
                 <div
-                    style={{
-                        textAlign: "center",
-                        fontSize: "0.9em"
-                }}>
+                    style={divStyle}>
                     <p>2018 &copy; Developed by Elena Novikova
                         <br></br>
                         for Udacity / Google FEND
