233b3d8a51731acf7e00605e0e6d52bcbed5b45d
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-08-28 12:51:42 +0100

    refactor: Choose books thumbnail

--- src/Book.js
+++ src/Book.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 
 class Book extends Component{
     render() {
+        // Checking if the book have proper thumbnail to display
+        let displayedThumbnail;
+        if (this.props.book.imageLinks) {
+            displayedThumbnail = this.props.book.imageLinks.thumbnail;
+        } else {
+            displayedThumbnail = '';
+        }
+
+        const thumbnailStyle = {
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${displayedThumbnail})`
+        };
+
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${ this.props.book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-cover" style={thumbnailStyle}></div>
                     <div className="book-shelf-changer">
-                        <select>
+                        <select
+                            defaultValue={this.props.book.shelf || 'none'}
+                            onChange={(event) => this.props.changeBookShelf(this.props.book, event.target.value)}
+                            value={this.props.currentShelf}
+                        >
                             <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
+                            <option value="wantToRead">Want to Read</option>
                             <option value="none">None</option>
                         </select>
                     </div>
