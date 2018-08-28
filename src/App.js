afb2bbdf02474e0928b60caf07122df489d39d6e
Elena Novikova <36162105+ElenaNovikova@users.noreply.github.com>
2018-08-28 12:40:08 +0100

    refactor: Fetch books

--- src/App.js
+++ src/App.js
@@ -1,49 +1,47 @@
 import React from 'react';
 import MainPage from './MainPage';
-import SearchPage from './SearchPage';
+import Search from './Search';
 import { Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import Footer from './Footer';
-import './App.css'
+import './App.css';
 
 class BooksApp extends React.Component {
+    // Let's store books data
     state = {
         books: []
     }
     // Receiving the initial set of books from backend server
     componentDidMount() {
+        this.fetchAllBooks();
+    }
+
+    fetchAllBooks = () => {
         BooksAPI.getAll().then((books) => {
             this.setState({ books })
         })
     }
 
-    moveShelf = (book, shelf) => {
+    // Updating the current book's shelf positioning
+    changeBookShelf = (book, shelf) => {
         BooksAPI.update(book, shelf);
-        BooksAPI.getAll().then((books) => {
-            this.setState({ books })
-        })
+        this.componentDidMount();
     }
 
     render() {
         return (
             <div className="app">
-              <Route exact path="/" render={() => (
-                  <MainPage
-                      books={this.state.books}
-                      moveShelf={this.moveShelf}
-                  />
-                )}
-              />
-              <Route path="/search" render={() => (
-                  <SearchPage
-                      moveShelf={this.moveShelf}
-                      books={this.state.books}
-                  />
-                )}
-              />
-              <Footer />
+                <Route exact path="/" render={() => (
+                    // Data is being passed through to the child component - MainPage
+                    <MainPage books={this.state.books} changeBookShelf={this.changeBookShelf} /> )}
+                />
+                <Route path="/search" render={() => (
+                    // Data is being passed through to the child component - Search
+                    <Search books={this.state.books} changeBookShelf={this.changeBookShelf} /> )}
+                />
+                <Footer />
             </div>
-        )
+        );
     }
 }
 
